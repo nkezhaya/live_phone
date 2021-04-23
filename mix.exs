265888f1b61c1f45e92bdfcfddecdcd36a380c57
Nick Kezhaya <nick@whitepaperclip.com>
2021-04-23 08:57:15 -0500

    0.3.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LivePhone.MixProject do
   def project do
     [
       app: :live_phone,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
