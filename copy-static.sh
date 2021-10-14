b30268185da491001a71b8c817c54231669b4dbc
Nick Kezhaya <nick@whitepaperclip.com>
2021-10-14 11:35:20 -0500

    Removed debug lines from copy-static

--- copy-static.sh
+++ copy-static.sh
@@ -5,11 +5,3 @@ cp assets/*.js \
    deps/phoenix/priv/static/*.js \
    deps/phoenix_live_view/priv/static/*.js \
    priv/assets/js
-
-echo $(pwd)
-
-echo $(ls)
-echo $(ls priv)
-echo $(ls priv/assets)
-echo $(ls priv/assets/js)
-echo $(ls _build/test/lib/live_phone/priv/assets/js)
