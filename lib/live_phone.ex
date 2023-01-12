045fb22738b8193745d3d485b689ffed3ce33839
Nick Kezhaya <nick@whitepaperclip.com>
2023-01-12 09:42:50 -0600

    Reformatted some comments, removed sort_by_name/2

--- lib/live_phone.ex
+++ lib/live_phone.ex
@@ -7,8 +7,8 @@ defmodule LivePhone do
   alias LivePhone.Countries
 
   @doc ~S"""
-  This is used to verify a given `phone` number and see if it is a valid
-  number according to ExPhoneNumber.
+  This is used to verify a given phone number and see if it is a valid number
+  according to ExPhoneNumber.
 
   ## Examples
 
@@ -82,16 +82,18 @@ defmodule LivePhone do
   end
 
   @doc ~S"""
-  This is used to normalize a given `phone` number to E.164 format,
-  and returns a tuple with `{:ok, formatted_phone}` for valid numbers
-  and `{:error, unformatted_phone}` for invalid numbers.
+  This is used to normalize a given `phone` number to E.164 format, and returns
+  a tuple with `{:ok, formatted_phone}` for valid numbers and `{:error,
+  unformatted_phone}` for invalid numbers.
 
   ## Examples
 
       iex> LivePhone.normalize("1234", nil)
       {:error, "1234"}
+
       iex> LivePhone.normalize("+1234", nil)
       {:ok, "+1234"}
+
       iex> LivePhone.normalize("+1 (650) 253-0000", "US")
       {:ok, "+16502530000"}
 
@@ -102,19 +104,16 @@ defmodule LivePhone do
     |> String.replace(~r/[^+\d]/, "")
     |> ExPhoneNumber.parse(country)
     |> case do
-      {:ok, result} ->
-        {:ok, result |> ExPhoneNumber.format(:e164)}
-
-      _ ->
-        {:error, phone}
+      {:ok, result} -> {:ok, ExPhoneNumber.format(result, :e164)}
+      _ -> {:error, phone}
     end
   end
 
   @doc ~S"""
-  Parses the given `country_code` into an emoji, but I should
-  note that the emoji is not validated so it might return an
-  invalid emoji (this will also depend on the unicode version
-  supported by your operating system, and which flags are included.)
+  Parses the given `country_code` into an emoji, but I should note that the
+  emoji is not validated so it might return an invalid emoji (this will also
+  depend on the unicode version supported by your operating system, and which
+  flags are included.)
 
   ## Examples
 
--- lib/live_phone/countries.ex
+++ lib/live_phone/countries.ex
@@ -1,8 +1,9 @@
 defmodule LivePhone.Countries do
   @moduledoc """
-  The `LivePhone.Countries` module can be used to list all known countries and return
-  them ordered alphabetically, and optionally you can ensure your preferred countries
-  are prepended to the list instead of mixed in with the rest.
+  The `LivePhone.Countries` module can be used to list all known countries and
+  return them ordered alphabetically, and optionally you can ensure your
+  preferred countries are prepended to the list instead of mixed in with the
+  rest.
   """
 
   alias ISO
@@ -14,8 +15,8 @@ defmodule LivePhone.Countries do
   This function returns all known countries as `LivePhone.Country` structs,
   sorted alphabetically by country name.
 
-  Optionally you can specify a list of preferred country codes, these will
-  subsequently be prepended to the list.
+  Optionally you can specify a list of preferred country codes, and these will
+  be put at the top of the list.
 
   ```elixir
   # This will return everything alphabetically
@@ -32,10 +33,13 @@ defmodule LivePhone.Countries do
     preferred = preferred |> Enum.uniq() |> Enum.with_index()
 
     ISO.countries()
-    |> Enum.map(&Country.from_iso/1)
-    |> Enum.map(&set_preferred_flag(&1, preferred))
+    |> Enum.map(fn country ->
+      country
+      |> Country.from_iso()
+      |> set_preferred_flag(preferred)
+    end)
     |> Enum.filter(&(&1.region_code && &1.region_code != ""))
-    |> Enum.sort(&sort_by_name/2)
+    |> Enum.sort_by(& &1.name)
     |> Enum.sort_by(&sort_by_preferred(&1, preferred), :desc)
   end
 
@@ -87,11 +91,6 @@ defmodule LivePhone.Countries do
       nil -> country
       {_, _index} -> %{country | preferred: true}
     end
-  end
-
-  @spec sort_by_name(%{name: String.t()}, %{name: String.t()}) :: boolean()
-  defp sort_by_name(%{name: name_1}, %{name: name_2}) do
-    name_1 < name_2
   end
 
   @spec sort_by_preferred(Country.t(), list(String.t())) :: integer()
